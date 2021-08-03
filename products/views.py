563b5231ef72d1034b31b8dad2231e12395723f5
smcgdub <stephen_xyz1@hotmail.com>
2021-08-03 20:18:48 +0000

    Final save for the night

--- products/views.py
+++ products/views.py
@@ -12,6 +12,10 @@ def all_products(request):
     categories = None
 
     if request.GET:
+        if 'sort' in request.GET:
+            if ''
+
+
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
             products = products.filter(category__name__in=categories)
