decfca2fbdd8c5bd36f099bce49381d6693954e5
smcgdub <stephen_xyz1@hotmail.com>
2021-08-12 18:02:56 +0000

    Added quantity boxes to shopping bage page

--- bag/views.py
+++ bag/views.py
@@ -1,51 +1,16 @@
-# from django.shortcuts import render, redirect
-
-# # # Create your views here.
-
-# def view_bag(request):
-#     """ A view to render the shopping bag contents """
-
-#     return render(request, 'bag/bag.html')
-
-
-# def add_to_bag(request, item_id):
-#     """ Add a quantity of the specified product to the shopping bag """
-#     quantity = int(request.POST.get('quantity'))
-#     redirect_url = request.POST.get('redirect_url')
-#     size = None
-#     if 'product_size' in request.POST:
-#         size = request.POST['product_size']
-#     bag = request.session.get('bag', {})
-
-#     if size:
-#         if item_id in list(bag.keys()):
-#             if size in bag[item_id]['items_by_size'].keys():
-#                 bag[item_id]['items_by_size'][size] += quantity
-#             else:
-#                 bag[item_id] = {'items_by_size': {size: quantity}}
-#     else:
-#         if item_id in list(bag.keys()):
-#             bag[item_id] += quantity
-#         else:
-#             bag[item_id] = quantity
-
-#     request.session['bag'] = bag
-#     return redirect(redirect_url)
-
-
-
 from django.shortcuts import render, redirect
 
 # Create your views here.
 
 def view_bag(request):
-    """ A view that renders the bag contents page """
+    """ A view to render the shopping bag contents """
 
     return render(request, 'bag/bag.html')
 
+
 def add_to_bag(request, item_id):
     """ Add a quantity of the specified product to the shopping bag """
-
+    
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     size = None
@@ -69,3 +34,41 @@ def add_to_bag(request, item_id):
 
     request.session['bag'] = bag
     return redirect(redirect_url)
+
+
+
+# from django.shortcuts import render, redirect
+
+# # Create your views here.
+
+# def view_bag(request):
+#     """ A view that renders the bag contents page """
+
+#     return render(request, 'bag/bag.html')
+
+# def add_to_bag(request, item_id):
+#     """ Add a quantity of the specified product to the shopping bag """
+
+#     quantity = int(request.POST.get('quantity'))
+#     redirect_url = request.POST.get('redirect_url')
+#     size = None
+#     if 'product_size' in request.POST:
+#         size = request.POST['product_size']
+#     bag = request.session.get('bag', {})
+
+#     if size:
+#         if item_id in list(bag.keys()):
+#             if size in bag[item_id]['items_by_size'].keys():
+#                 bag[item_id]['items_by_size'][size] += quantity
+#             else:
+#                 bag[item_id]['items_by_size'][size] = quantity
+#         else:
+#             bag[item_id] = {'items_by_size': {size: quantity}}
+#     else:
+#         if item_id in list(bag.keys()):
+#             bag[item_id] += quantity
+#         else:
+#             bag[item_id] = quantity
+
+#     request.session['bag'] = bag
+#     return redirect(redirect_url)
